5ace89a24c37421d1918449095a0281b9df9308b
Erick Branner <eobrann@gmail.com>
2020-09-24 16:41:59 -0700

    changed file structure and andded file header

---
+++ assignment1-movies/asn1.c
@@ -0,0 +1,16 @@
+/*********************************************************************
+* ** Program Filename: asn1.c
+* ** Author: Erick Branner
+* ** Date: 24 September 2020
+* ** Description: Contains main function for assignment
+* ** Input: CSV Files
+* ** Output: Strings of text
+* *********************************************************************/
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+}
