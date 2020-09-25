b8401f1c44fa5c214ffc713ba3f1cd4b90bbac26
Erick Branner <eobrann@gmail.com>
2020-09-24 22:12:44 -0700

    get_int() returns correct integer; is int reprompts for space delimted strings

--- assignment1-movies/prog.c
+++ assignment1-movies/prog.c
@@ -16,14 +16,18 @@ unsigned int is_int(char* str){
     }
     return 1;
 }
-
+// Used for reference on reading a string with spaces:
+// Also used for clearing the input buffer
+// https://www.includehelp.com/c/c-program-to-read-string-with-spaces-using-scanf-function.aspx
 int get_int(char* prompt, int hi, int low){
-
-    printf("%s",prompt);
+    char input[32]; // statically allocated for now
+    char tmp; //for clearing the input buffer
     do{
-        printf("loop");
-    }while(is_int(prompt)==0);
-    return -1; 
+        printf("%s", prompt);
+        scanf("%[^\n]", &input);
+        scanf("%c",&tmp);
+    }while(is_int(input)==0);
+    return atoi(input); 
 }
 
 void show_options(){
@@ -35,7 +39,9 @@ void show_options(){
 }
 
 void main_loop(){
+    int foo = 0;
     show_options();
-    get_int("Prompt:",1,4);
+    foo = get_int("Prompt:",1,4);
+    printf("Foo: %d\n", foo);
     return;
 }
