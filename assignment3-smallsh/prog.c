eb064564c52cbd1cb55d51fdb299f111ca88d141
Erick Branner <eobrann@gmail.com>
2020-10-23 21:34:27 -0700

    all memory freed; one memory error

--- assignment3-smallsh/prog.c
+++ assignment3-smallsh/prog.c
@@ -9,10 +9,11 @@
 #include "./prog.h"
 
 #define BUFF_SIZE 2048
-#define MAX_ARGS 512
+#define MAX_ARGS 513 /* +1 from max to add null*/
 
 struct cmd_line{
 	char* args[MAX_ARGS];	
+	int len;
 
 };
 
@@ -21,31 +22,42 @@ struct cmd_line{
 /* Null terminated string */
 // might need one more command line arg space (for null)
 struct cmd_line* cmd_line_process(char* line){
-	  struct cmd_line* output = malloc(sizeof(struct cmd_line));
-
-    // For use with strtok_r
-    char* saveptr = NULL;
-		char* flagptr = line; 
-		char* token;
-    int i; 
-		// the first token is the title
-		/* 	Set flagptr to null after first iteration to ensure strtok 
-				works for the remaining interations*/
-    for(	i = 0, flagptr = line;
-					token != NULL; flagptr = NULL, i++)
-		{
-			
-			token = strtok_r(flagptr, " \n", &saveptr);
-			if(token != NULL){
-				output->args[i] = calloc(strlen(token)+1, sizeof(char));
-				strcpy(output->args[i], token);
-				// fprintf(stdout, "args[%d]: %s\n", i, output->args[i]);
-			}
-
+	struct cmd_line* output = malloc(sizeof(struct cmd_line));
+	output->len = 0;
+	// maybe set output->args to null
+	// For use with strtok_r
+  char* saveptr = NULL;
+	char* flagptr = line; 
+	char* token;
+	int i; 
+	
+	// the first token is the title
+	/* 	Set flagptr to null after first iteration to ensure strtok 
+			works for the remaining interations*/
+  for(	i = 0, flagptr = line;
+				token != NULL; flagptr = NULL, i++)
+	{
+		
+		token = strtok_r(flagptr, " \n", &saveptr);
+		if(token != NULL){
+			output->args[i] = calloc(strlen(token)+1, sizeof(char));
+			strcpy(output->args[i], token);
+			output->len++;
+			// fprintf(stdout, "args[%d]: %s\n", i, output->args[i]);
 		}
+	}
+	fprintf(stdout, "len: %d\n", output->len);
 	return output;
 }
 
+void cmd_line_free(struct cmd_line* c){
+
+	for(int i = 0; i < c->len; i++){
+		if(c->args[i] != NULL) free(c->args[i]);
+	}
+	if(c != NULL) free(c);
+	return; 
+}
 
 void main_proc(){
 	char PS1[] = ": ";
@@ -60,12 +72,9 @@ void main_proc(){
 		getline(&buff, &buffsize, stdin);
 		foo = cmd_line_process(buff);
 		if(strcmp(foo->args[0], "exit") == 0) ex = 1;
-		free(foo->args);
-		free(foo);
+		cmd_line_free(foo);
 	}
 	
-
-	free(foo);
 	free(buff);
 	return;
 }
