193a6d9423222348409cac80fb65a138908d2456
Erick Branner <eobrann@gmail.com>
2020-11-02 21:01:40 -0800

    toggle on global var foreground only mode with ^Z

--- assignment3-smallsh/prog.c
+++ assignment3-smallsh/prog.c
@@ -11,6 +11,9 @@
 #include "./command.h"
 #include "./globals.h"
 #include "./child_proc.h"
+
+int fg_only = false; 
+
 // #include "./signal.h"
 
 // #define BUFF_SIZE 2048
@@ -19,7 +22,21 @@
 /* SIGNAL HANDLERS */
 
 void handle_SIGINT(int signo){
-	char* message = "smallsh caught SIGINT; ignoring\n";
+	char* message = "smallsh: caught SIGINT; ignoring\n";
+  // We are using write rather than printf
+	write(STDOUT_FILENO, message, strlen(message));
+	fflush(stdout);
+	// sleep(10);
+}
+void handle_SIGTSTP(int signo){
+	char* message = NULL; 
+	if(fg_only == true){
+		fg_only = false;
+		message = "smallsh: DISABLED: foreground only mode \n";
+	}else{
+		fg_only = true;
+		message = "smallsh: ENABLED: foreground only mode \n";
+	} 
   // We are using write rather than printf
 	write(STDOUT_FILENO, message, strlen(message));
 	fflush(stdout);
@@ -38,7 +55,6 @@ void main_proc(){
 	struct child_proc* children = NULL;
 	children = child_proc_create();
 	int catch = -1;
-
 	/* Install signal handlers */
 
 	/* SIGINT */
@@ -48,6 +64,12 @@ void main_proc(){
 	SIGINT_action.sa_flags = 0;
 	sigaction(SIGINT, &SIGINT_action, NULL);
 
+	/* SIGSTP */
+	struct sigaction SIGTSTP_action = {{0}};
+	SIGTSTP_action.sa_handler = handle_SIGTSTP;
+	sigfillset(&SIGTSTP_action.sa_mask);
+	SIGTSTP_action.sa_flags = 0;
+	sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
 
 	/****************************/
