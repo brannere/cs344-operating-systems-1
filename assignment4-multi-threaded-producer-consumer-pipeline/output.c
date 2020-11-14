f420007c7d3ef89054aacbcfb6c57fdb2c919e92
Erick Branner <eobrann@gmail.com>
2020-11-14 15:32:57 -0800

    outputs in lines of 80

--- assignment4-multi-threaded-producer-consumer-pipeline/output.c
+++ assignment4-multi-threaded-producer-consumer-pipeline/output.c
@@ -8,6 +8,7 @@
 
 /* deletes history when printed */
 void write_line(struct input* in){
+	int i = 0;
 
 	if(strlen(in->history) % OUTPUT_LEN == 0){
 		fprintf(stdout, "printing multiple of %d\n", OUTPUT_LEN);
@@ -16,21 +17,25 @@ void write_line(struct input* in){
 		/* Clean history */
 		memset(in->history, '\0', strlen(in->history));
 	}
-	else if(strlen(in->history) < 80){
+	else if(strlen(in->history) < OUTPUT_LEN){
 		fprintf(stdout, "less OUTPUT_LEN; do nothing\n");
 	}
 	else{
 		fprintf(stdout, "\nOUTPUT\n");
 		for
 		(
-			int i = 0;
+			i = 0;
 			i < (strlen(in->history) - ((strlen(in->history))%OUTPUT_LEN));
 			i++
 		)
 		{
+			if(i % OUTPUT_LEN == 0) fprintf(stdout, "\n");
 			fprintf(stdout, "%c", in->history[i]);
 		} 
 		fprintf(stdout, "\n");
+		fprintf(stdout, "i ended with: %d\n", i);
+		i++;
+		fprintf(stdout, "last char seen: %c\n", in->history[i]);
 	}
 
 	return; 
