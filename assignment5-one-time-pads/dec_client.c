44129761ffebbd3fac85451838453bb5f649be7b
Erick Branner <eobrann@gmail.com>
2020-11-19 14:46:58 -0800

    makefile for project 5

---
+++ assignment5-one-time-pads/dec_client.c
@@ -0,0 +1,17 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * Function: main()
+ * Description: main process for program
+ * Parameters: argument count and argument number
+ * Pre-Conditions: csv file passed from command line exists
+ * Post-Conditions: None
+ */
+int main(){
+
+	return EXIT_SUCCESS;                   
+}
---
+++ assignment5-one-time-pads/main.c
@@ -0,0 +1,27 @@
+/**
+ * Prgram Filename: main.c
+ * Author: Erick Branner
+ * Date: 16 November 2020
+ * Description: Contains main function/process for assginment
+ * Input:
+ * Output: 
+ * 
+*/
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * Function: main()
+ * Description: main process for program
+ * Parameters: argument count and argument number
+ * Pre-Conditions: csv file passed from command line exists
+ * Post-Conditions: None
+ */
+int main(){
+
+	return EXIT_SUCCESS;                   
+}
